c4334eee3059a08230f6c3670cc7d82cc99ad592
root <root@4pfg.l.time4vps.cloud>
2025-08-31 17:30:18 +0300

    REV_2

--- the_snake.py
+++ the_snake.py
@@ -15,6 +15,7 @@ DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 DIRECTIONS = (UP, DOWN, LEFT, RIGHT)
+INITIAL_POSITION = (0, 0)
 
 # Цвета
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
@@ -25,6 +26,18 @@ WHITE_COLOR = (255, 255, 255)
 
 # Скорость змейки
 SPEED = 3
+
+# -------------------------------------------------------------
+TRANSITIONS = {
+    (pg.K_UP, LEFT): UP,
+    (pg.K_UP, RIGHT): UP,
+    (pg.K_DOWN, LEFT): DOWN,
+    (pg.K_DOWN, RIGHT): DOWN,
+    (pg.K_LEFT, UP): LEFT,
+    (pg.K_LEFT, DOWN): LEFT,
+    (pg.K_RIGHT, UP): RIGHT,
+    (pg.K_RIGHT, DOWN): RIGHT,
+}
 
 # Включение экрана
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -41,7 +54,7 @@ class GameObject:
         self.body_color = body_color
 
     def draw(self):
-        """Нарисовать обьект."""
+        """Нарисовать объект."""
         raise NotImplementedError
 
 
@@ -51,16 +64,16 @@ class Apple(GameObject):
     def __init__(self):
         """Обозначение случайной позиции."""
         super().__init__(APPLE_COLOR)
-        self.position = (0, 0)
+        # self.position = INITIAL_POSITION
 
     def randomize_position(self, occupied_positions):
         """Случайное положение яблока вне змейки."""
         while True:
-            new_position = (
+            self.position = (
                 randint(0, GRID_WIDTH - 1) * GRID_SIZE,
-                randint(0, GRID_HEIGHT - 1) * GRID_SIZE)
-            if new_position not in occupied_positions:
-                self.position = new_position
+                randint(0, GRID_HEIGHT - 1) * GRID_SIZE,
+            )
+            if self.position not in occupied_positions:
                 break
 
     def draw(self):
@@ -71,18 +84,17 @@ class Apple(GameObject):
 
 
 class Snake(GameObject):
-    """Контроль зиейки игроком."""
+    """Контроль змейки игроком."""
 
     def __init__(self):
         """Вывести змейку на середину экрана."""
         super().__init__(SNAKE_COLOR)
         self.reset()
-        self.direction = choice(DIRECTIONS)
+        self.direction = RIGHT
         self.next_direction = None
-        self.grow = False
 
     def get_head_position(self):
-        """Возратить позицию головы змейки."""
+        """Возвратить позицию головы змейки."""
         return self.positions[0]
 
     def update_direction(self):
@@ -93,11 +105,11 @@ class Snake(GameObject):
 
     def move(self):
         """Шаг вперёд змейки."""
-        head_x, head_y = self.positions[0]
+        head_x, head_y = self.get_head_position()
         dx, dy = self.direction
         new_head = (
             (head_x + dx * GRID_SIZE) % SCREEN_WIDTH,
-            (head_y + dy * GRID_SIZE) % SCREEN_HEIGHT
+            (head_y + dy * GRID_SIZE) % SCREEN_HEIGHT,
         )
 
         self.positions.insert(0, new_head)
@@ -107,9 +119,9 @@ class Snake(GameObject):
             self.grow = False
 
     def reset(self):
-        """Обновление позиции змейки в первоночальное положение."""
+        """Обновление позиции змейки в первоначальное положение."""
         self.positions = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
-        self.direction = choice([UP, DOWN, LEFT, RIGHT])
+        self.direction = RIGHT
         self.grow = False
 
     def draw(self):
@@ -121,25 +133,15 @@ class Snake(GameObject):
 
 
 def handle_keys(snake):
-    """Ручное управление змейкой через клавиатуру с  использованием словаря."""
-    opposites = {UP: DOWN, DOWN: UP, LEFT: RIGHT, RIGHT: LEFT}
-    key_to_direction = {
-        pg.K_UP: UP,
-        pg.K_DOWN: DOWN,
-        pg.K_LEFT: LEFT,
-        pg.K_RIGHT: RIGHT
-    }
-
+    """Ручное управление змейкой через клавиатуру с использованием TRANSITIONS."""
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             raise SystemExit
         elif event.type == pg.KEYDOWN:
-            if event.key in key_to_direction:
-                new_dir = key_to_direction[event.key]
-                if (new_dir != snake.direction
-                        and new_dir != opposites[snake.direction]):
-                    snake.next_direction = new_dir
+            new_dir = TRANSITIONS.get((event.key, snake.direction))
+            if new_dir:
+                snake.next_direction = new_dir
 
 
 def main():
@@ -156,16 +158,15 @@ def main():
         snake.move()
 
         # Проверка столкновения с самим собой
-        head_pos = snake.get_head_position()
-        if head_pos in snake.positions[1:]:
+        if snake.get_head_position() in snake.positions[1:]:
             snake.reset()
 
-        # Проверка ест ли змея яблоко
+        # Проверка, ест ли змея яблоко
         if snake.get_head_position() == apple.position:
             snake.grow = True
             apple.randomize_position(snake.positions)
 
-        # Показать всё на дисплее.
+        # Показать всё на дисплее
         screen.fill(BOARD_BACKGROUND_COLOR)
         snake.draw()
         apple.draw()
